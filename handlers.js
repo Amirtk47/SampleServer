8103ef66f8d7b4abfdea0749426f1799e347fd2c
Amir <amirho3eint47@gmail.com>
2022-08-16 11:25:11 +0000

    Adding new handlers for testing local storage

--- handlers.js
+++ handlers.js
@@ -1,5 +1,16 @@
 
-export function registerRoutes(app) {
+export function registerRoutes(app, localStorage) {
+
+  app.get("/Send", (req, res) => {
+    let value = req.query.value;
+    localStorage.setItem("SampleValue", value);
+    res.status(200).send("saved");
+  });
+
+  app.get("/Receive", (req, res) => {
+    let value = localStorage.getItem("SampleValue");
+    res.send(value);
+  });
   
   app.get("/Users", (req, res) => {
     console.log("here");
--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express = require("express");
 var registerRoutes = require("./handlers");
 
 var app = express();
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require("node-localstorage").LocalStorage;
+  localStorage = new LocalStorage("./storage");
+}
 
-registerRoutes(app);
+registerRoutes(app, localStorage);
 
 var listener = app.listen(8080, function () {
   console.log("Listening on port " + listener.address().port);
