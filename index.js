ef4b1d8d1e6036fb40481b6fdfbf962fe2c66511
edrishn <edrishn@gmail.com>
2022-08-16 12:10:34 +0430

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ import opine from "https://deno.land/x/opine@2.2.0/mod.ts";
 
 var app = opine();
 
-app.get("/Users", (req, res) => {
-  console.log("here");
-  res.send([{ Name: "Person1" }, { Name: "Person2" }]);
-});
-app.get("/", (req, res) => {
-  console.log("here");
-  res.send("Welcome!");
-});
+import {registerRoutes} from "./handlers"
+
+registerRoutes(app);
 
 var listener = app.listen(8080, function () {
   console.log("Listening on port " + listener.address().port);
