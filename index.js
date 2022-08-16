0ceb0426bc546075684eb967991fc9c2fbf194ce
Amir <amirho3eint47@gmail.com>
2022-08-16 09:12:36 +0000

    Fix: index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import opine from "https://deno.land/x/opine@2.2.0/mod.ts";
 
 var app = opine();
 
-import {registerRoutes} from "./handlers"
+import { registerRoutes } from "./handlers.js";
 
 registerRoutes(app);
 
-var listener = app.listen(8080, function () {
-  console.log("Listening on port " + listener.address().port);
+app.listen(8080, function () {
+  console.log("Listening on port 8080 ");
 });
