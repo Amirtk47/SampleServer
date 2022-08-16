7123993829ea259f6f7510ad2e84cc98fff3d15c
edrishn <edrishn@gmail.com>
2022-08-16 12:09:30 +0430

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ var router = express.Router();
 import {registerRoutes} from "./handlers"
 
 registerRoutes(router);
+
+var listener = app.listen(8080, function () {
+  console.log("Listening on port " + listener.address().port);
+});
